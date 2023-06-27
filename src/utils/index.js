ccd035b5fdee1ae6b063563ffaa3c0647d45f38c
ramawow <rmt0276@gmail.com>
2023-06-27 18:43:50 +0700

    edit update create

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,14 +1,17 @@
-const mapDBToModel = ({ id, name, title, year, performer, genre, duration, album_id, playlist_id, user_id }) => ({
-  id,
-  name,
-  title,
-  year,
-  performer,
-  genre,
-  duration,
-  albumId: album_id,
-  playlistId: playlist_id,
-  userId: user_id,
-});
+const mapDBToModel = ({ id, name, title, year, performer, genre, duration, album_id, playlist_id, created_at,
+  updated_at, user_id }) => ({
+    id,
+    name,
+    title,
+    year,
+    performer,
+    genre,
+    duration,
+    albumId: album_id,
+    playlistId: playlist_id,
+    createdAt: created_at,
+    updatedAt: updated_at,
+    userId: user_id,
+  });
 
 module.exports = { mapDBToModel };
